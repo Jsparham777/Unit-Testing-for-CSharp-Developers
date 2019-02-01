66a3d464537bff9dab762707c3749f5448cf4dfc
James Sparham <jsparham777@gmail.com>
2019-02-01 17:18:03 +0000

    Dependency injection via method parameters

--- TestNinja/Program.cs
+++ TestNinja/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TestNinja.Mocking;
 
 namespace TestNinja
 {
@@ -10,6 +6,10 @@ namespace TestNinja
     {
         static void Main(string[] args)
         {
+            var service = new VideoService();
+            var title = service.ReadVideoTitle(new FileReader());
+
+            //Instead of newing up the objects here, use a dependency injection framework (NInject)
         }
     }
 }
